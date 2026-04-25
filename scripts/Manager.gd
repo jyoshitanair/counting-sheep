cf1ff94a19d5b970df36534bb9a5cac028fcb1c9
evelynhyx <evebabyhua@gmail.com>
2026-04-24 23:41:16 -0500

    working on leaderboard

--- scripts/Manager.gd
+++ scripts/Manager.gd
@@ -1,3 +1,22 @@
 extends Node
 var player_name: String
 var player_list = []
+var score = 0
+
+func _ready() -> void:
+	SilentWolf.configure({
+		"api_key": "API_KEY",
+		"game_id": "countingsheep",
+		"log_level": "1",
+	})
+
+	SilentWolf.configure_scores({
+		"open_scene_on_close": "res://scenes/main.tscn"
+	})
+
+func _physics_process(delta: float) -> void:
+	leaderboard()
+
+func leaderboard():
+	for score in Manager.score:
+		Manager.player_list.append(Manager.player_name)
--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -1,26 +1,31 @@
 extends CharacterBody2D
 var direction = Vector2(-1,0)
 const speed = 2300
+var alive = true
+var score = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("player")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta) -> void:
-	if Input.is_action_just_pressed("space"):
-		print("return to menu")
-	if Input.is_action_pressed("up"):
-		print("up")
-		direction = Vector2(0,-1)
-	if Input.is_action_pressed("down"):
-		print("down")
-		direction = Vector2(0,1)
-	if Input.is_action_pressed("left"):
-		direction = Vector2(-1,0)
-	if Input.is_action_pressed("right"):
-		direction = Vector2(1,0)
-	velocity = direction*speed*delta
-	move_and_slide()
-	
+	if alive:
+		if Input.is_action_just_pressed("space"):
+			print("return to menu")
+		if Input.is_action_pressed("up"):
+			print("up")
+			direction = Vector2(0,-1)
+		if Input.is_action_pressed("down"):
+			print("down")
+			direction = Vector2(0,1)
+		if Input.is_action_pressed("left"):
+			direction = Vector2(-1,0)
+		if Input.is_action_pressed("right"):
+			direction = Vector2(1,0)
+		velocity = direction*speed*delta
+		move_and_slide()
+	else:
+		Manager.score = score
+		
 
 func _on_area_2d_mouse_entered() -> void:
 	print("hello")
