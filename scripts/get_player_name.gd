4b44644cbe4123a9566ca1fac8ec6dffe5104237
Jyoshita <jyoshitanair123@gmail.com>
2026-04-25 01:45:08 -0700

    testing failed. recovered a file

--- scripts/get_player_name.gd
+++ scripts/get_player_name.gd
@@ -7,11 +7,3 @@ func _on_submit_pressed() -> void:
 	var sw_result : Dictionary = await SilentWolf.Scores.save_score(Manager.player_name, Manager.score).sw_save_score_complete
 	print ("Score persisted successfully: " + str(sw_result.score_id))
 	self.hide()
-
-## Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
-
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-	#pass
