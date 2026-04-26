bc21804b76c0b3e662f4783452cdd7411f71cfa4
ch0colatt3 <sabrinawu6412@gmail.com>
2026-04-26 11:16:46 -0500

    progress

--- scripts/summoners.gd
+++ scripts/summoners.gd
@@ -1,11 +1,28 @@
-extends StaticBody2D
+
+extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
+
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+
+
+signal on_touch
 func _ready() -> void:
-	pass # Replace with function body.
+	add_to_group("collectable")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if(body.is_in_group("player") and body.alive):
+	
+		emit_signal("on_touch")
+		call_deferred("_disabled_collision")
+func _disabled_collision() -> void:
+	collision_shape_2d.disabled = true
+	
+# Called when the node enters the scene tree for the first time.
